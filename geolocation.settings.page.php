b2e6124242990e6003327f879b8bd2f7120f20a3
ymichel <ymichel@github.com>
2023-02-13 12:57:39 +0100

    tidy functions

--- geolocation.settings.page.php
+++ geolocation.settings.page.php
@@ -6,9 +6,9 @@ function geolocation_settings_page()
     }
 
     default_settings();
-    echo '<link rel="stylesheet" href="' . get_osm_leaflet_css_url() . '"/>';
-    echo '<script src="' . get_osm_leaflet_js_url() . '"></script>';
 ?>
+    <link rel="stylesheet" href="<?php echo get_osm_leaflet_css_url(); ?>"/>
+    <script src="<?php echo get_osm_leaflet_js_url(); ?>"></script>
     <style type="text/css">
         #preload {
             display: none;
@@ -54,8 +54,7 @@ function geolocation_settings_page()
                 <td class="position">
                     <input type="radio" id="geolocation_map_position_before" name="geolocation_map_position" value="before" <?php is_value('geolocation_map_position', 'before'); ?>><label for="geolocation_map_position_before"><?php _e('Before the post.', 'geolocation'); ?></label><br />
                     <input type="radio" id="geolocation_map_position_after" name="geolocation_map_position" value="after" <?php is_value('geolocation_map_position', 'after'); ?>><label for="geolocation_map_position_after"><?php _e('After the post.', 'geolocation'); ?></label><br />
-                    <input type="radio" id="geolocation_map_position_shortcode" name="geolocation_map_position" value="shortcode" <?php is_value('geolocation_map_position', 'shortcode'); ?>><label for="geolocation_map_position_shortcode"><?php _e('Wherever I put the shortcode: ', 'geolocation');
-                                                                                                                                                                                                                                                echo esc_attr((string) get_option('geolocation_shortcode')); ?>.</label>
+                    <input type="radio" id="geolocation_map_position_shortcode" name="geolocation_map_position" value="shortcode" <?php is_value('geolocation_map_position', 'shortcode'); ?>><label for="geolocation_map_position_shortcode"><?php _e('Wherever I put the shortcode: ', 'geolocation'); echo esc_attr((string) get_option('geolocation_shortcode')); ?>.</label>
                 </td>
             </tr>
             <tr valign="top">
